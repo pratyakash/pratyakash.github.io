d1d9effa2341ddbaa0f88bc7b2cc534dfd5e231e
pratyakash <pratyakash13@gmail.com>
2019-01-15 03:53:53 +0530

    Particle Background

--- pratyakash.github.io/src/App.js
+++ pratyakash.github.io/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './resources/style.css';
 import Header from './components/header_footer/Header';
+import Particle from './components/Particle/Particle';
+
+import Intro from './components/Introduction/Intro'
+
 
 class App extends Component {
   render() {
     return (
       <div>
-       <Header />
+        <Particle />
+        <Header />
+        <Intro />
       </div>
     );
   }
---
+++ pratyakash.github.io/src/components/Introduction/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            sdwdwqwdqwdqwdwqdqwdwqdqwdwqdwq
+        </div>
+    );
+};
+
+export default Intro;
