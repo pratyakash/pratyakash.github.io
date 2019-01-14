3a8c07f5222518d46b9aa9fd6e8e1f917e0f5708
pratyakash <pratyakash13@gmail.com>
2019-01-15 02:34:24 +0530

    Add Header

---
+++ pratyakash.github.io/src/components/header_footer/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                
+                <div className="nav">
+                    
+                    <div className="nav-l">
+                        <div className="title_wrap">
+                            <div className="title font_righteous">Pratyakash Saini</div>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
