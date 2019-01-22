b1b791d38ebabe86c34e13c10b21037752b7f051
pratyakash <pratyakash13@gmail.com>
2019-01-22 22:33:24 +0530

    Ipdating

--- pratyakash.github.io/src/components/Introduction/Intro.js
+++ pratyakash.github.io/src/components/Introduction/Intro.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Intro = () => {
-    return (
-        <div className="intro">
-            sdwdwqwdqwdqwdwqdqwdwqdqwdwqdwq
-        </div>
-    );
-};
+class Intro extends Component {
+    render() {
+        return (
+            <div className="intro">
+                <div className="name">
+                    Pratyakash Saini
+                </div>
+                <div className="description">
+                    <h1>Front End Developer based in India.</h1>
+                    <h3>I believe in creating fantastic user experience with miniliasm</h3>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Intro;
