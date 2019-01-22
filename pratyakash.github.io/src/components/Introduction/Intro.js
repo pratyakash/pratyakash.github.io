756cdbbeab8c31621043c9b3be7aca0e8fbf01e2
pratyakash <pratyakash13@gmail.com>
2019-01-23 01:37:39 +0530

    first page

--- pratyakash.github.io/src/components/Introduction/Intro.js
+++ pratyakash.github.io/src/components/Introduction/Intro.js
@@ -8,12 +8,12 @@ class Intro extends Component {
                     Pratyakash Saini
                 </div>
                 <div className="description">
-                    <h1>Hi, I am Front End Developer <span className="special">#ReactJS </span>creates website that run across platforms & devices</h1>
+                    <h1>Hi, I am Front End Developer <a className="special" href="https://reactjs.org/" target="_black" >#ReactJS </a>creates website that run across platforms & devices</h1>
                 </div>
                 <div className="connect">
-                    <a href>Gmail</a>
-                    <a href>Github</a>
-                    <a href>Resume</a>
+                    <a href>#Gmail</a>
+                    <a href>#Github</a>
+                    <a href>#Curriculum Vitae</a>
                 </div>
             </div>
         );
