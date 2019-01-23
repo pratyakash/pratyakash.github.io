cb77d41a212a415332725af39993d0218ddeb5a9
pratyakash <pratyakash13@gmail.com>
2019-01-23 22:04:39 +0530

    Projects

---
+++ pratyakash.github.io/src/components/Projects/project.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render() {
+        return (
+            <div>
+                <div className="project_body">
+                    <div className="project_list">
+                        <div className="project_image">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Project;
