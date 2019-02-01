d5d06ce63d1cab34e9c1738b29725a6783932c37
pratyakash <pratyakash13@gmail.com>
2019-02-01 21:59:52 +0530

    Adding JS

--- pratyakash.github.io/src/components/Skills/skills.js
+++ pratyakash.github.io/src/components/Skills/skills.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import htmlico from '../../resources/images/html.png';
 import cssico from '../../resources/images/css.png';
+import jsico from '../../resources/images/js.png';
 
 class skills extends Component {
     state={
-        icon:[htmlico,cssico]
+        icon:[htmlico,cssico,jsico]
     }
     render() {
         return (
