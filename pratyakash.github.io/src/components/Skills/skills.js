93cab92765a3a6da958ad1cd4e1d201c86fc8533
pratyakash <pratyakash13@gmail.com>
2019-02-01 21:49:02 +0530

    State

---
+++ pratyakash.github.io/src/components/Skills/skills.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class skills extends Component {
+    state={
+        icon:[]
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default skills;
