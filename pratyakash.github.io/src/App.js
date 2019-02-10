5e4b4ebfd80c675a4d9008703218fe44f5e65e27
pratyakash <pratyakash13@gmail.com>
2019-02-10 22:20:56 +0530

    Intro up

--- pratyakash.github.io/src/App.js
+++ pratyakash.github.io/src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         <Intro />
-        <Projects />
+        {/* <Projects /> */}
         {/* <Skills /> */}
       </div>
     );
