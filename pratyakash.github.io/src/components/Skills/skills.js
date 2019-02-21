b8fa1477b9d6479d9890a4d07a8607dadd426138
pratyakash <pratyakash13@gmail.com>
2019-02-21 21:50:32 +0530

    sass icon

--- pratyakash.github.io/src/components/Skills/skills.js
+++ pratyakash.github.io/src/components/Skills/skills.js
@@ -3,10 +3,11 @@ import htmlico from '../../resources/images/html.png';
 import cssico from '../../resources/images/css.png';
 import jsico from '../../resources/images/js.png';
 import bsico from '../../resources/images/bs.png';
+import sasico from '../../resources/images/bs.png';
 
 class Skills extends Component {
     state={
-        icon:[htmlico,cssico,jsico,bsico],
+        icon:[htmlico,cssico,jsico,bsico,sasico],
         showExp: false
     }
 
